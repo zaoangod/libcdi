4e6fddc409e2311f956635a4935ac2744fe37317
jiayan <zaoangod@qq.com>
2025-12-28 02:10:57 +0800

    init

--- cli/cli.c
+++ cli/cli.c
@@ -335,7 +335,7 @@ int main(int argc, char* argv[])
 
 			cJSON_AddStringToObject(volumeItem, "Volume", Ucs2ToUtf8(p->VolPath));
 			// cJSON_AddNumberToObject(volumeItem, "StartLBA", startLba);
-			// cJSON_AddNumberToObject(volumeItem, "PartitionNumber", p->PartNum);
+			cJSON_AddNumberToObject(volumeItem, "PartitionNumber", p->PartNum);
 			cJSON_AddStringToObject(volumeItem, "PartitionType", Ucs2ToUtf8(p->PartType));
 			cJSON_AddStringToObject(volumeItem, "PartitionID", Ucs2ToUtf8(p->PartId));
 			cJSON_AddStringToObject(volumeItem, "BootIndicator", p->BootIndicator ? "Yes" : "No");
